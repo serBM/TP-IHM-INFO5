abcb99543520717727788c494662d6f0e4237626
vincemax <max4vin17@gmail.com>
2019-10-07 11:41:14 +0200

    drag 2 buttons in a window with no collision

--- TP1/src/controller/Controller.java
+++ TP1/src/controller/Controller.java
@@ -1,5 +1,55 @@
 package controller;
 
-public class Controller {
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+
+public class Controller implements MouseListener {
+
+	Point pStart;
+	Point pEnd;
+	JButton bMin;
+	JButton bMax;
+
+	public Controller(JButton b1, JButton b2) {
+		bMin = b1;
+		bMax = b2;
+		bMin.addMouseListener(this);
+		bMax.addMouseListener(this);
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		if(arg0.getSource() == bMin) {
+			pStart = bMin.getLocation();
+			System.out.println("bMin - Mouse pressed : " + pStart);
+		}
+		else {
+			pStart = bMax.getLocation();
+			System.out.println("bMax - Mouse pressed : " + pStart);
+		}
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		pEnd = arg0.getPoint();
+		if(arg0.getSource() == bMin) {
+			bMin.setLocation(pEnd.x + pStart.x, pStart.y);
+			System.out.println("bMin - Mouse released : " + pEnd);
+		}
+		else {
+			bMax.setLocation(pEnd.x + pStart.x, pStart.y);
+			System.out.println("bMax - Mouse released : " + pEnd);
+		}
+	}
 
 }
--- TP1/src/views/View.java
+++ TP1/src/views/View.java
@@ -1,5 +1,36 @@
 package views;
 
-public class View {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import controller.Controller;
+
+//CTRL + SHIFT + O pour générer les imports
+@SuppressWarnings("serial")
+public class View extends JFrame {
+
+	private JPanel pan = new JPanel();
+	private JButton bMin = new JButton("1");
+	private JButton bMax = new JButton("2");
+	private Controller c = new Controller(bMin, bMax);
+
+	public View() {
+		this.setLayout(null);
+		pan.setLayout(null);
+		this.setTitle("Test");
+		this.setSize(400, 400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		bMin.setBounds(0, 200, 10, 30);
+		bMax.setBounds(200, 200, 10, 30);
+		pan.add(bMin);
+		pan.add(bMax);
+		this.setContentPane(pan);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		View v = new View();
+	}
 }
