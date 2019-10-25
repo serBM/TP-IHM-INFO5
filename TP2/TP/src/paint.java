eafd204b47c9475032b13fbc7a5736d83368a080
Jade Vandal <jade.vandal@gmail.com>
2019-10-25 10:18:06 +0200

    tools ok

--- TP2/TP/src/paint.java
+++ TP2/TP/src/paint.java
@@ -260,7 +260,14 @@ class Paint extends JFrame {
 			public void mousePressed(MouseEvent me) {
 				System.out.println(me);
 				if(me.getButton() == MouseEvent.BUTTON3) {
+					  
 		        	  System.out.println("Right Click!");
+		        	  setLayout(null);
+		        	  
+		        	  JButton jb = new JButton("test");
+		        	  jb.setLocation(me.getX(), me.getY());
+		        	  add(jb);
+		        	  panel.repaint();
 		          }
 			}
 	    
